9d9085b74bbee4095b0347e735dc35af5b7bf089
pcardoso <pcardoso@ualg.pt>
2025-03-11 12:15:36 +0000

    improvements on the flask scripts

--- 2-WebAPI/Flask-API/library/app_01_a_first_get_and_post.py
+++ 2-WebAPI/Flask-API/library/app_01_a_first_get_and_post.py
@@ -28,19 +28,25 @@ things_counter = 2
 
 @app.errorhandler(404)
 def not_found(error):
+    # return an error message and the HTTP status code 404, which HTTP defines as the code for "Not Found".
+    # this is a custom error message, called when the requested resource is not found.
     return jsonify({'error': 'Not found'}), 404
 
 
 @app.route('/iot/api/v1.0/things', methods=['GET'])
 def get_things():
-    return jsonify(things)
+    # return the list of things and the HTTP status code 200, which HTTP defines as the code for "OK".
+    # 200 is the default status code returned by a server when a request is successful.
+    return jsonify(things), 200
 
 
 @app.route('/iot/api/v1.0/things/<int:thing_id>', methods=['GET'])
 def get_thing(thing_id):
     try:
+        # return the thing with the given id and the HTTP status code 200, which HTTP defines as the code for "OK".
         return jsonify(things[thing_id]), 200
     except:
+        # return an empty JSON object and the HTTP status code 404, which HTTP defines as the code for "Not Found".
         return jsonify({}), 404
 
 
@@ -63,7 +69,8 @@ def create_thing():
     }
     things[things_counter] = new_thing
 
-    # status code 201, which HTTP defines as the code for "Created". See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    # return the new thing and the HTTP status code 201, which HTTP defines as the code for "Created".
+    # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
     return jsonify({things_counter: new_thing}), 201
 
 
@@ -77,6 +84,7 @@ def update_thing(thing_id):
         thing['name'] = request.json.get('name', thing['name'])
         thing['local'] = request.json.get('local', thing['local'])
         thing['sensors'] = request.json.get('sensors', thing['sensors'])
+        # return the updated thing and status code 200, which HTTP defines as the code for "OK
         return jsonify({thing_id: things[thing_id]}), 200
     except:
         abort(404)  # 404 Not Found
@@ -87,6 +95,7 @@ def delete_thing(thing_id):
     try:
         thing = things[thing_id]
         del things[thing_id]
+        # return the deleted thing and status code 200, which HTTP defines as the code for "OK".
         return jsonify({thing_id: thing}), 200
     except:
         abort(404)  # 404 Not Found
--- 2-WebAPI/Flask-API/try_API.py
+++ 2-WebAPI/Flask-API/try_API.py
@@ -3,16 +3,29 @@ import requests
 uri = 'http://localhost:8080/iot/api/v1.0/things'
 
 print(20 * '*' + ' GET all')
+print(uri)
+
 response = requests.get(uri)
+
+print(20 * '*' + " Response:")
 print(response.text)
 
+input("it <enter> to continue...")
+# ------------------------------------------------
 
 print(20 * '*' + ' GET one')
+print(uri + '/1')
+
 response = requests.get(uri + '/1')
+
+print(20 * '*' + " Response:")
 print(response.text)
 
+input("it <enter> to continue...")
+# ------------------------------------------------
 
 print(20 * '*' + ' POST')
+print(uri)
 data = {
     "local": "@lab. 163 / ISE /UAlg",
     "name": "Apollo",
@@ -27,20 +40,33 @@ data = {
         }
     ]
 }
+print("data:", data)
+
 response = requests.post(uri, json=data)
+print(20 * '*' + " Response:")
 print(response.text)
 
+input("it <enter> to continue...")
+# ------------------------------------------------
 
 print(20 * '*' + ' PUT')
+print(uri + '/3')
+print("data:", {"name": "Apollo X1"})
 response = requests.put(uri + '/3',
                         json={"name": "Apollo X1"})
+
+print(20 * '*' + " Response:")
 print(response.text)
+
 print(20 * '*' + ' GET  ALL (after put)')
 response = requests.get(uri)
 print(response.text)
 
+input("it <enter> to continue...")
+# ------------------------------------------------
 
 print(20 * '*' + ' DELETE')
+print(uri + '/3')
 response = requests.delete(uri + '/3')
 print(response.text)
 
@@ -48,6 +74,3 @@ print(response.text)
 print(20 * '*' + ' GET  ALL (after delete)')
 response = requests.get(uri)
 print(response.text)
-
-
-
